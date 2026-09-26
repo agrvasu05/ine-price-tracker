27f38c2b03ea3592bdc0d60e9090e1aec5dbc332
Vasu Agrawal <157293981+agrvasu05@users.noreply.github.com>
2026-09-26 17:39:06 +0530

    Change URL path validation from '/product/' to '/item/'

--- backend/src/utils/storeUrl.js
+++ backend/src/utils/storeUrl.js
@@ -3,7 +3,7 @@ import { env } from "../config/env.js";
 export function normalizeAndValidateStoreUrl(value) {
   if (typeof value !== "string" || !value.trim()) throw new Error("Product URL is required");
   const url = new URL(value, env.storeBaseUrl);
-  if (url.origin !== new URL(env.storeBaseUrl).origin || !/^\/product\/[1-9]\d*$/.test(url.pathname) || url.username || url.password) {
+  if (url.origin !== new URL(env.storeBaseUrl).origin || !/^\/item\/[1-9]\d*$/.test(url.pathname) || url.username || url.password) {
     throw new Error("Use an INE mock-store product URL");
   }
   return url.origin + url.pathname;
